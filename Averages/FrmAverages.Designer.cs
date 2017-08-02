ca2658e5cfe12364034ebd96a9ecbfa783385d58
AJ Speller <ajspeller@gmail.com>
2017-08-02 15:06:23 -0400

    Add average code

---
+++ Averages/FrmAverages.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Averages
+{
+    partial class FrmAverages
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAverages = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnAverages
+            // 
+            this.BtnAverages.Location = new System.Drawing.Point(68, 190);
+            this.BtnAverages.Name = "BtnAverages";
+            this.BtnAverages.Size = new System.Drawing.Size(161, 23);
+            this.BtnAverages.TabIndex = 0;
+            this.BtnAverages.Text = "Determine Averages";
+            this.BtnAverages.UseVisualStyleBackColor = true;
+            this.BtnAverages.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmAverages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.BtnAverages);
+            this.Name = "FrmAverages";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Averages";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAverages;
+    }
+}
+
---
+++ Averages/FrmAverages.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Averages
+{
+    public partial class FrmAverages : Form
+    {
+        public FrmAverages()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mark1;
+            int mark2;
+            int mark3;
+            double average;
+
+            mark1 = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter a mark", "Averages", "0"));
+            mark2 = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter a mark", "Averages", "0"));
+            mark3 = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter a mark", "Averages", "0"));
+
+            average = (double)(mark1 + mark2 + mark3) / 3;
+
+            MessageBox.Show("Average: " + average.ToString());
+
+        }
+    }
+}
