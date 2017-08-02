cea03dc79ea1e8e62b6204a533b7195fb1e5d672
AJ Speller <ajspeller@gmail.com>
2017-08-02 15:44:00 -0400

    Answer excercises

--- Averages/FrmAverages.Designer.cs
+++ Averages/FrmAverages.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.BtnAverages = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BtnAverages
             // 
-            this.BtnAverages.Location = new System.Drawing.Point(68, 190);
+            this.BtnAverages.Location = new System.Drawing.Point(32, 12);
             this.BtnAverages.Name = "BtnAverages";
             this.BtnAverages.Size = new System.Drawing.Size(161, 23);
             this.BtnAverages.TabIndex = 0;
@@ -41,11 +43,33 @@
             this.BtnAverages.UseVisualStyleBackColor = true;
             this.BtnAverages.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Problem 6";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(79, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Problem 7";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // FrmAverages
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.BtnAverages);
             this.Name = "FrmAverages";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -57,6 +81,8 @@
         #endregion
 
         private System.Windows.Forms.Button BtnAverages;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
